1d63ddd261f8e4eb905b6ac5c1921db772570ea3
Mujtaba-Ali21 <mujtabaaliprince730@gmail.com>
2023-12-11 17:45:39 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ app.post('/send-notification', ( req, res ) => {
 })
 
 app.listen(port, () => {
-  console.log(`Face Call listening on port ${port}`)
+  console.log(`Face Call listening on Port ${port}`)
 })
